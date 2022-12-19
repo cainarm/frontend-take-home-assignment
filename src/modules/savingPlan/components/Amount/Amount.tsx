a1fae2d1dc0058b68fdbb5b5709a4a9843b0926b
cainarm <cainarm@yahoo.com.br>
2022-12-19 17:03:49 -0300

    fix: fixed wrong input identifier in field

--- src/modules/savingPlan/components/Amount/Amount.tsx
+++ src/modules/savingPlan/components/Amount/Amount.tsx
@@ -3,5 +3,5 @@ import { CurrencyInput } from 'modules/common/components/Input';
 type Props = Omit<React.ComponentProps<typeof CurrencyInput>, 'label'>;
 
 export function Amount(props: Props) {
-  return <CurrencyInput label="Total amount" name="ReachDate" {...props} />;
+  return <CurrencyInput label="Total amount" name="amount" {...props} />;
 }
